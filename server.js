3f23e4b2abff16d5c5be1a0fe9977d3da66e59aa
Debanjan Das <microsoft2420@gmail.com>
2025-03-09 10:20:18 +0530

    server regenrated

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 import express from 'express';
 import path from 'path';
-import { fileURLToPath } from 'url';// to render the login.html file 
+import { fileURLToPath } from 'url'; // to render the login.html file 
 
-const app = express() ;
+const app = express();
 const port = 3000;
 
 // Resolve the __dirname (since __dirname is not available in ES modules)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve static files from the "styling" folder
+// Serve static files from the "login" folder
+app.use(express.static(path.join(__dirname, 'login')));
 app.use(express.static(path.join(__dirname, 'styling')));
-
-
 
 // Route to serve login.html
 app.get('/', (req, res) => {
-    return res.sendFile(path.join(__dirname, 'login.html'));
+    console.log("server connected");
+    return res.sendFile(path.join(__dirname, 'login', 'login.html'));
 });
 
 app.listen(port, () => {
     console.log(`Server started at ${port}`);
-})
+});
