0b96232b57e014c4f7484d8c500041cd79e2ac90
Debanjan Das <microsoft2420@gmail.com>
2025-03-10 13:45:34 +0530

    server updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url'; // to render the login.html file 
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url"; // to render the login.html file
 
 const app = express();
 const port = 3000;
@@ -10,15 +10,24 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve static files from the "login" folder
-app.use(express.static(path.join(__dirname, 'login')));
-app.use(express.static(path.join(__dirname, 'styling')));
+app.use(express.static(path.join(__dirname, "login")));
+app.use(express.static(path.join(__dirname, "styling")));
+app.use(express.static(path.join(__dirname, "home_page")));
 
 // Route to serve login.html
-app.get('/', (req, res) => {
-    console.log("server connected");
-    return res.sendFile(path.join(__dirname, 'login', 'login.html'));
+app.route("/login")
+.get((req, res) => {
+  console.log("server connected");
+  return res.sendFile(path.join(__dirname, "login", "login.html"));
+})
+.post((req , res) => {
+    
+})
+
+app.route("/").get((req, res) => {
+  return res.sendFile(path.join(__dirname, "home_page", "index.html"));
 });
 
 app.listen(port, () => {
-    console.log(`Server started at ${port}`);
+  console.log(`Server started at ${port}`);
 });
